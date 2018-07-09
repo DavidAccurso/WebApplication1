fcc2d5d24178b54c85820da3832e4018dd71c89c
DavidAccurso <davidmaccurso@gmail.com>
2018-07-09 02:34:11 -0300

    Agregar archivos de proyecto.

---
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Producto",
+                url: "Buscar/{nombreProducto}",
+                defaults: new { controller = "Producto", action = "Buscar", nombreProducto = "" }
+            );
+
+            routes.MapRoute(
+                name: "Buscar",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Producto", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+             name: "Default",
+             url: "{controller}/{action}/{id}",
+             defaults: new { controller = "Persona", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApplication1/Controllers/PersonaController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class PersonaController : Controller
+    {
+        // GET: Persona
+        public ActionResult Persona()
+        {
+            Models.Persona p = new Models.Persona();
+            p.apellido = Request.Form["apellido"];
+            p.nombre = Request.Form["nombre"];
+            p.edad = int.Parse(Request.Form["edad"]);
+            return View(p);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
